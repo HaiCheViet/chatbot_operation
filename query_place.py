f24ecc8a2486e4fcfe14898e067a6b4d80a03d26
Khai Doan Duy <khaidoan25@gmail.com>
2019-06-29 17:35:15 +0700

    modify query place

--- query_place.py
+++ query_place.py
@@ -10,15 +10,47 @@ class QueryPlace(object):
             data = json.load(f)['Place']
             return data
             
-    def query_place(self, state, service=None, cost=None, capability=None, area=None):
+    def query_place_state(self, state: str, cost=False, capability=False):
         return_places = []
         for place in self.data:
             if place["location"]["state"] == state:
                 return_places.append(place)
-        
+        if cost:
+            return sorted(return_places, key=lambda x: x["cost"])[:3]
+        if capability:
+            return sorted(return_places, key=lambda x: x["capability"], reverse=True)[:3]
         return return_places
+
+    def query_place_service(self, service: float):
+        return_places = []
+        for place in self.data:
+            if place["service"] >= service:
+                return_places.append(place)
+        return sorted(return_places, key=lambda x: x["service"], reverse=True)[:3]
+
+    def query_place_cost(self, cost: int):
+        return_places = []
+        for place in self.data:
+            if place["cost"] <= cost:
+                return_places.append(place)
+        return sorted(return_places, key=lambda x: x["cost"])[:3]
+
+    def query_place_capability(self, capability: int):
+        return_places = []
+        for place in self.data:
+            if place["capability"] >= capability:
+                return_places.append(place)
+        return sorted(return_places, key=lambda x: x["capability"], reverse=True)[:3]
+
+    def query_place_area(self, area: int):
+        return_places = []
+        for place in self.data:
+            if place["area"] >= area:
+                return_places.append(place)
+        return sorted(return_places, key=lambda x: x["area"], reverse=True)[:3]
 
 if __name__ == "__main__":
     query = QueryPlace()
-    result = query.query_place("New York, United States")
+    result = query.query_place_state("New York, United States", cost=True)
+    # result = query.query_place_cost(1500)
     print(result)
