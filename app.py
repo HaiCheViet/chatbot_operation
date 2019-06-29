e7bede1b9ca3cea2e36c1998f351ff2bbc7ade70
qui <nvqui97@gmail.com>
2019-06-29 23:58:06 +0700

    init new action

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 
+import response
 from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
@@ -18,10 +19,59 @@ def webhook():
     # build request object
     req = request.get_json(force=True)
     # get action from json
-    # action = req.get('queryResult').get('action')
+    action = req.get('queryResult').get('action')
     intent = req.get('queryResult').get('intent')["displayName"]
-    # intent = req.get('intent').get('query_investor_intent')
-    # print(req)
+    para = req.get('queryResult').get('parameters')
+
+    fulfillment = response.fulfillment_response()
+    Slack = response.SlackResponse()
+    if action == 'setup':
+
+        pass
+    
+    elif action == 'send_mail':
+        '''
+        @para:  typeofperson: [member, investor]
+                rule: [content, appointment, reminder]
+                (mail): mail
+        '''
+        if para['mail'] == '':
+            # send mail to one person
+            pass
+        else:
+            # send mail to a group
+            pass
+
+    elif action == 'add_info':
+        '''
+        @para:  rule: [member, investor]
+                (name): name
+        '''
+        if para['name'] == '' and para['rule'] == 'member':
+            # add member from csv file (get from google sheet)            
+            pass
+        elif para['name'] == '' and para['rule'] == 'investor':
+            # request name of investor
+            # response context  investor_follow
+            pass
+        elif para['name'] =! '' and para['rule'] == 'member':
+            # request slot for member
+            # response context member_follow
+            pass
+        else:
+            # reply done
+            pass
+
+    elif action == 'add_info_investor':
+        pass
+    elif action == '':
+        pass
+    elif action == '':
+        pass
+    elif action == '':
+        pass
+    
+
     pprint(req)
     print(intent)
 
