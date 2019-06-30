4ee2de946c9e989f3f0c6aaf8a6ebd1f4ef1435a
Khai Doan Duy <khaidoan25@gmail.com>
2019-06-30 10:05:30 +0700

    modify place_database, query_place.py

--- query/query_place.py
+++ query/query_place.py
@@ -9,40 +9,55 @@ class QueryPlace(object):
         with open("data/place_database.json", "r", encoding="utf-8") as f:
             data = json.load(f)['Place']
             return data
+
+    def query_place_info_by_name(self, name):
+        for place in self.data:
+            if place["name"] = name:
+                return place
+
+    def query_place_email_by_name(self, name):
+        for place in self.data:
+            if place["name"] == name:
+                return place["email"]
             
-    def query_place_state(self, state: str, cost=False, capability=False):
+    def query_place_by_state(self, state: str, cost=False, capability=False):
+        temp_return_places = []
         return_places = []
         for place in self.data:
             if place["location"]["state"] == state:
-                return_places.append(place)
+                temp_return_places.append(place)
         if cost:
-            return sorted(return_places, key=lambda x: x["cost"])[:3]
+            temp_return_places = sorted(temp_return_places, key=lambda x: x["cost"])[:3]
         if capability:
-            return sorted(return_places, key=lambda x: x["capability"], reverse=True)[:3]
+            temp_return_places = sorted(temp_return_places, key=lambda x: x["capability"], reverse=True)[:3]
+        
+        for place in temp_return_places:
+            return_places.append(place["name"])
+
         return return_places
 
-    def query_place_service(self, service: float):
+    def query_place_by_service(self, service: float):
         return_places = []
         for place in self.data:
             if place["service"] >= service:
                 return_places.append(place)
         return sorted(return_places, key=lambda x: x["service"], reverse=True)[:3]
 
-    def query_place_cost(self, cost: int):
+    def query_place_by_cost(self, cost: int):
         return_places = []
         for place in self.data:
             if place["cost"] <= cost:
                 return_places.append(place)
         return sorted(return_places, key=lambda x: x["cost"])[:3]
 
-    def query_place_capability(self, capability: int):
+    def query_place_by_capability(self, capability: int):
         return_places = []
         for place in self.data:
             if place["capability"] >= capability:
                 return_places.append(place)
         return sorted(return_places, key=lambda x: x["capability"], reverse=True)[:3]
 
-    def query_place_area(self, area: int):
+    def query_place_by_area(self, area: int):
         return_places = []
         for place in self.data:
             if place["area"] >= area:
